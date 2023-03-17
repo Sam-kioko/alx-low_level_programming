cf3c4253f666282ce1f1b88df50dbd053bc21b5c
Sam-kioko <kiokosamuel@zetech.ac.ke>
2023-03-16 18:06:31 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Results: 0 on success
+ */
+
+int main(void)
+{
+printf("The size of char is %zu bytes.\n", sizeof(char));
+printf("The size of int is %zu bytes.\n", sizeof(int));
+printf("The size of long int is %zu bytes.\n", sizeof(long int));
+printf("The size of long long int is %zu bytes.\n", sizeof(long long int));
+printf("The size of float is %zu bytes.\n", sizeof(float));
+return (0);
+}
