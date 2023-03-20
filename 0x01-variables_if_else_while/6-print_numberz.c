f59944ca7c644ffd6db965389711332fa4438dcd
Sam-kioko <kiokosamuel@zetech.ac.ke>
2023-03-20 08:50:02 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base ten from zero
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar (i + '0');
+	}
+	putchar ('\n');
+
+	return (0);
+}
