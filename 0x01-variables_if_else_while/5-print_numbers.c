1bcdd79608b9c470dbb7f860a652535c0ef0374a
Sam-kioko <kiokosamuel@zetech.ac.ke>
2023-03-20 08:21:42 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digits of base ten from zero
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
