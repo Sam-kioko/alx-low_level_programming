617ae10677c50c40b7c6a85155919cf46f01f18b
Sam-kioko <kiokosamuel@zetech.ac.ke>
2023-03-20 09:30:52 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base sixteen in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 10; i <= 10; i++)
+	{
+		putchar (i);
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar (i);
+	}
+	putchar ('\n');
+
+	return (0);
+}
