37c15089112c26ab9b868772a6a44b4f24729739
Sam-kioko <kiokosamuel@zetech.ac.ke>
2023-03-20 14:09:21 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combination of two digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+
+	for (b = a + 1; b <= '9'; b++)
+	{
+	if (b != a)
+	{
+		putchar (a);
+		putchar (b);
+	if (a == '8' && b == '9')
+	continue;
+	putchar (',');
+	putchar (' ');
+	}
+	}
+	}
+	putchar ('\n');
+
+	return (0);
+}
