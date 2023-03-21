7a273f75aa3617fda8c7cbef14b943c3698cdca9
Sam-kioko <kiokosamuel@zetech.ac.ke>
2023-03-21 13:12:42 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - prints the nine times table
+ */
+
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+	for (j = 1; j <= 9; j++)
+	{
+		k = i * j;
+
+	if (k <= 9)
+	{
+		_putchar(' ');
+		_putchar(k + '0');
+	}
+	else
+	{
+		_putchar((k / 10) + '0');
+		_putchar((k % 10) + '0');
+	}
+
+	if (j < 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+	}
+
+	_putchar('\n');
+	}
+}
