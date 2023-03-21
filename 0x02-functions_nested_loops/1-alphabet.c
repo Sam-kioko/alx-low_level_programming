e149b8aff46a831c6833f58c8b67495c2d6f277a
Sam-kioko <kiokosamuel@zetech.ac.ke>
2023-03-21 07:00:37 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
