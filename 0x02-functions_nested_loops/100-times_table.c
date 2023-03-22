1f97366864a39afb1ce4b9de0d98abac64dde0d7
Sam-kioko <kiokosamuel@zetech.ac.ke>
2023-03-22 14:53:37 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,9 +12,11 @@ void print_times_table(int n)
 
 	if (n < 0 || n > 15)
 	{
-
+		return;
+	}
 	for (i = 0; i <= n; i++)
 	{
+		_putchar('0');
 		for (j = 0; j <= n; j++)
 		{
 		prod = i * j;
@@ -45,6 +47,5 @@ void print_times_table(int n)
 		}
 		}
 		_putchar('\n');
-	}
 	}
 }
