5394c638a5bfc01e69da416b0dbd69f3114092d9
Sam-kioko <kiokosamuel@zetech.ac.ke>
2023-03-27 08:46:16 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,14 @@
 #ifndef main_h
 # define main_h
 
-int _putchar(char c);
+int _putchar(char);
 
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
 
 int _strlen(char *s);
+
+void _puts(char *str);
 
 #endif
