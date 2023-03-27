e9108885a04f6d4dfc1ec8c1afb661ca7d6a31b5
Sam-kioko <kiokosamuel@zetech.ac.ke>
2023-03-27 07:20:43 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of an integer to 98
+ * @n: a pointer to an integer
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
