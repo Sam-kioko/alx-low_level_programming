2e31e1cd9169dfdc63ac81e291dd74aab87088a3
Sam-kioko <kiokosamuel@zetech.ac.ke>
2023-04-10 14:57:01 -0700

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: counts the command-line arguments
+ * @argv: the array containing command-line arguments
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
