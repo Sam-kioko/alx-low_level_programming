dff22459b6d7ba1aefd3fb234fe688509427d200
Sam-kioko <kiokosamuel@zetech.ac.ke>
2023-04-17 04:19:28 -0700

    Structure dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+# define DOG_H
+
+/**
+ * struct dog - contains 3 members
+ * @name: pointer to a charachter
+ * @age: float number
+ * @owner: pointer to a charachter
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
