1c2b4a9b5cef23427020dd332ed685b1e09fc9d1
Sam-kioko <kiokosamuel@zetech.ac.ke>
2023-05-03 09:35:08 -0700

    function that prints a text before  the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * startup_print - prints a text before the main function is executed
+ *
+ * Return: void
+ */
+
+void startup_print(void)
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -30,4 +30,6 @@ list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
 
+void startup_print(void) __attribute__ ((constructor));
+
 #endif
